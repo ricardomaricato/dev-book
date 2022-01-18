13e2b50ca1684c6f58585e47c4d4ca442ad532a3
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-17 22:21:53 -0300

    feat: inserindo rotas dentro do router

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,9 +1,12 @@
 package router
 
 import (
+	"api/src/router/rotas"
+
 	"github.com/gorilla/mux"
 )
 
 func Gerar() *mux.Router {
-	return mux.NewRouter()
+	r := mux.NewRouter()
+	return rotas.Configurar(r)
 }
