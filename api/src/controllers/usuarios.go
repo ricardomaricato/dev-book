97808989b2ffdeae27ab12197e702e6340372d74
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-18 21:02:26 -0300

    feat: criando repositorios

--- api/src/controllers/usuarios.go
+++ api/src/controllers/usuarios.go
@@ -1,10 +1,37 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 // CriarUsuario insere um usuário no banco de dados
 func CriarUsuario(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Crindo Usuário"))
+	corpoRequest, erro := ioutil.ReadAll(r.Body)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		log.Fatal(erro)
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuario.ID, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		log.Fatal(erro)
+	}
 }
 
 // BuscarUsuarios busca todos os usuários salvos no banco
---
+++ api/src/repositorios/usuarios.go
@@ -0,0 +1,21 @@
+package repositorios
+
+import (
+	"api/src/modelos"
+	"database/sql"
+)
+
+// Usuarios representa um repositório de usuarios
+type Usuarios struct {
+	db *sql.DB
+}
+
+// NovoRepositorioDeUsuarios cria um repositório de usuários
+func NovoRepositorioDeUsuarios(db *sql.DB) *Usuarios {
+	return &Usuarios{db}
+}
+
+// Criar insere um usuário no banco de dados
+func (repositorio Usuarios) Criar(usuario modelos.Usuario) (uint64, error) {
+	return 0, nil
+}
