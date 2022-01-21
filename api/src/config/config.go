461dea8c566d2a15728a58a7b9811a8aafc4fec9
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-20 21:36:01 -0300

    feat: validando token

--- api/src/config/config.go
+++ api/src/config/config.go
@@ -15,6 +15,9 @@ var (
 
 	// Porta onde a API vai estar rodando
 	Porta = 0
+
+	// SecretKey é a chave que vai ser usada para assinar o token
+	SecretKey []byte
 )
 
 // Carregar vai inicializar as variáveis de ambiente
@@ -36,4 +39,5 @@ func Carregar() {
 		os.Getenv("DB_NOME"),
 	)
 
+	SecretKey = []byte(os.Getenv("SECRET_KEY"))
 }
