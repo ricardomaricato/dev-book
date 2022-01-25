a522449b2fee1bae252244a39d68143164452b1e
Ricardo Maricato <ricardo.maricato@hotmail.com>
2022-01-24 23:53:23 -0300

    feat: buscar seguidores

--- api/src/repositorios/usuarios.go
+++ api/src/repositorios/usuarios.go
@@ -190,3 +190,36 @@ func (repositorio Usuarios) PararDeSeguir(usuarioID, seguidorID uint64) error {
 	return nil
 
 }
+
+// BuscarSeguidores traz todos os seguidores de um usuário
+func (repositorio Usuarios) BuscarSeguidores(usuarioID uint64) ([]modelos.Usuario, error) {
+	linhas, erro := repositorio.db.Query(`
+		select u.id, u.nome, u.nick, u.email, u.criadoEm
+		from usuarios u inner join seguidores s on u.id = s.seguidor_id where s.usuario_id = ?`,
+		usuarioID,
+	)
+	if erro != nil {
+		return nil, erro
+	}
+	defer linhas.Close()
+
+	var usuarios []modelos.Usuario
+	for linhas.Next() {
+		var usuario modelos.Usuario
+
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Nome,
+			&usuario.Nick,
+			&usuario.Email,
+			&usuario.CriadoEm,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
+
+}
